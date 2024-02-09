a8f8f5e9e356271203beb77072c55c4f3b6fcb38
kocpeel <134693584+kocpeel@users.noreply.github.com>
2024-02-09 11:33:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,25 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+
+
+#     Model użytkownika dodawany w aplikacji powinien być zgodny z następującym JSON:
+# {
+#     "firstName": str,
+#     "lastName": str,
+#     "birthYear": int,
+#     "group": str, // jedynymi poprawnymi wartościami są napisy: "user", "premium", "admin"
+   
+# }
+
+# Model użytkownika zwracany z aplikacji powinien być zgodny z następującym JSON:
+# {
+#     "id": int,
+#     "firstName": str,
+#     "lastName": str,
+#     "age": int,
+#     "group": str, // jedynymi poprawnymi wartościami są napisy: "user", "premium", "admin"
+   
+# }
+
 users = {}
