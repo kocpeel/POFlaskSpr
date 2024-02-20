d69f448e917441f86a531a813edaffe5697a22f8
kocpeel <134693584+kocpeel@users.noreply.github.com>
2024-02-20 12:56:35 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -8,3 +8,10 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
+
+
+def test_get_all_users(client):
+    response = client.get('/users')
+    assert response.status_code ==  200
+    data = json.loads(response.data)
+    assert isinstance(data['users'], dict)
