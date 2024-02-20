cd945e7cdfe53d1825e81b47f8e855e7dc5a61a3
kocpeel <134693584+kocpeel@users.noreply.github.com>
2024-02-20 12:39:53 +0100

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,3 +1,10 @@
 import pytest
 import json
 from app import app, users
+
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
